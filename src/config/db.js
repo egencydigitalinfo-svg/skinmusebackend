4b5f70cb8a066e86632ef68d7034fe5211515537
Your Name <youremail@example.com>
2026-04-19 15:09:02 +0500

    cheange db name

--- src/config/db.js
+++ src/config/db.js
@@ -3,9 +3,10 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://syedumerhassni:naibtana123@cluster0.8kun6ji.mongodb.net/beauty_store_testing?retryWrites=true&w=majority&appName=Cluster0", {
-      dbName: 'beauty_store',
+      dbName: 'beauty_store_testing',
     });
     console.log('✅ MongoDB Connected');
+    console.log("Mongo URI:", process.env.MONGODB_URI);
   } catch (error) {
     console.error('❌ MongoDB Connection Failed:', error.message);
     process.exit(1);
